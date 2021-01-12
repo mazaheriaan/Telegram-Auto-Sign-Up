f86af1028dc16b39a3453fbcb42242c7f0fc56db
Morteza <mortezasaki91@gmail.com>
2021-01-12 10:57:54 +0330

    Add DownloadFile

--- utility.py
+++ utility.py
@@ -13,3 +13,16 @@ def SwitchDesktop(desktop_number : int):
         pyautogui.hotkey('winleft','pagedown')
     else:
         pyautogui.hotkey('winleft','pageup')
+
+def DownloadFile(address : str, account : str, fileName : str):
+    if validators.url(address):        
+        req = requests.get(address)
+
+        if req.status_code == 200:
+            dest_file = '{0}/Accounts/{1}/{2}'.format(os.getcwd(), account,fileName)
+            logging.info("Save file on {0}/Accounts/{1}/{2}".format(os.getcwd(), account,fileName))
+            with open(dest_file,'wb') as f:
+                f.write(req.content)
+        return True
+    else:
+        return False
