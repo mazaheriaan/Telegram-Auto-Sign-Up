985bf0f15c0c3cfdfe1132e04085ab67b0f84837
Morteza <mortezasaki91@gmail.com>
2021-01-12 09:49:55 +0330

    Add switch desktop

--- utility.py
+++ utility.py
@@ -1,4 +1,5 @@
 import os
+import pyautogui
 
 def CreateDirectory(path : str):
     try:
@@ -6,3 +7,9 @@ def CreateDirectory(path : str):
         return True
     except:
         return False
+
+def SwitchDesktop(desktop_number : int):
+    if desktop_number == 1:
+        pyautogui.hotkey('winleft','pagedown')
+    else:
+        pyautogui.hotkey('winleft','pageup')
