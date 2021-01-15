b94a2bbbf698196b64f20dbda377c79a814ba900
Morteza <mortezasaki91@gmail.com>
2021-01-15 19:54:56 +0330

    Add Word2Number function

--- utility.py
+++ utility.py
@@ -29,3 +29,18 @@ def DownloadFile(address : str, account : str, fileName : str):
         return True
     else:
         return False
+
+def Word2Number(word : str):
+    """ Convert word number to number
+
+    Args:
+        word (str): your word
+
+    >>>print(Word2Number('five one one once again your code is one one five one one goodbye'))
+    5 1 1 once again your code is 1 1 5 1 1 goodbye
+    """
+    units = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight","nine"]
+    for (i, w) in enumerate(units, start = 0):
+        word = word.replace(w, str(i))
+    
+    return word
