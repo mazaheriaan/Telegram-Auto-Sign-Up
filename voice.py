4f89de8eb30c28121354b711b7bd44c824f8e2df
Morteza <mortezasaki91@gmail.com>
2021-01-15 21:10:00 +0330

    fix bug of unclosed file

--- voice.py
+++ voice.py
@@ -5,13 +5,31 @@ import os
 class Voice:
     def __init__(self, sound_address : str):
         if os.path.exists(sound_address) and sound_address.endswith('.wav'):
-            self.sound = AudioSegment.from_wav(sound_address)
+            with open(sound_address, 'rb') as f:
+                self.sound = AudioSegment.from_wav(f)
         else:
             raise "Invalid file"
 
     def Cut(self, start : float = 0, end : float = -1):
         self.sound = self.sound[start:end]
+        return len(self.sound)
+
+    def Boost(self, value : int = 8):
+        self.sound = self.sound + value
 
     def Save(self, output : str, format : str = 'wav' ):
-        self.sound.export(output,format=format)
+        out = self.sound.export(output,format=format)
+        out.close()
+        return output
 
+    def Recognize(self, sound_address : str):
+        api_key = 'HVET3LMFZFTAX55M3NQMHORLEMIWFE25'
+
+        r = sr.Recognizer()
+        with sr.AudioFile(sound_address) as source:
+            audio_text = r.listen(source)
+                
+            # using wit speech recognition
+            r.adjust_for_ambient_noise(source, duration=1)
+            text = r.recognize_wit(audio_text, key= api_key)
+            return text
