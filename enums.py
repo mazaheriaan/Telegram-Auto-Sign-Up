fe0c87b8fa48178f19ffc07b98f76775a2eb7224
Morteza <mortezasaki91@gmail.com>
2021-01-24 22:00:44 +0330

    First commit

--- enums.py
+++ enums.py
@@ -1,15 +1,15 @@
 import enum
 
 class RegisterAPIStatus(enum.Enum):
-    Succesfull : 200
-    AlreadyJoined : 201
+    Succesfull = 200
+    AlreadyJoined = 201
 
 class Gender(enum.Enum):
-    Man : 0
-    Woman : 1
+    Man = 0
+    Woman = 1
 
 class TelegramRegisterStats(enum.Enum):
-    Succesfull :  1
-    Ban : 2
-    Flood : 3
-    HasPassword : 4
+    Succesfull = 1
+    Ban = 2
+    Flood = 3
+    HasPassword = 4
